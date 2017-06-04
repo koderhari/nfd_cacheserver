1d35ba4753a9172f9587749fe217c113588ca64d
koderhari <koderhari@gmail.com>
2017-06-04 09:47:57 +0300

    create server and contracts

---
+++ cacheserverdemo/CacheServer.Contracts/Services/ICacheServer.cs
@@ -0,0 +1,19 @@
+﻿using NFX.Glue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheServer.Contracts.Services
+{
+    [Glued]
+    public interface ICacheServer:IDisposable
+    {
+        byte[] AddOrGetExisiting(string key, byte[] value);
+        bool Remove(string key);
+        byte[] Get(string key);
+        void Set(string key, byte[] value);
+        bool Contains(string key);
+    }
+}
---
+++ cacheserverdemo/CacheServer.Server/Services/CacheServer.cs
@@ -0,0 +1,52 @@
+﻿using CacheServer.Contracts.Services;
+using NFX.ApplicationModel.Pile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheServer.Server.Services
+{
+    public class CacheServer : ICacheServer
+    {
+        private Dictionary<string, PilePointer> keyPilePointersStore = new Dictionary<string, PilePointer>();
+        private IPile _pile;
+        public NFX.OS.ManyReadersOneWriterSynchronizer RWSynchronizer;
+
+        public CacheServer(IPile pile)
+        {
+            _pile = pile;
+        }
+
+        public byte[] AddOrGetExisiting(string key, byte[] value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Get(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(string key, byte[] value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
